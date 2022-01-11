fd48f1ba8de2b4774eb7d26396e041e243abe601
gthomp98 <71700861+gthomp98@users.noreply.github.com>
2022-01-11 02:57:34 +0000

    Design Finished, just commenting left

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,56 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Card, CardMedia } from "@mui/material";
+import { Button } from "@mui/material";
 const About = props => {
+  const theme = createTheme();
+  return (
 
-    return (
-      <div>
-        <h2>About</h2>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+
+        <Grid 
+        item 
+        xs={12} 
+        sm={8} 
+        md={12} 
+        component={Card} 
+        sx={{
+          backgroundImage: 'url(https://images7.alphacoders.com/868/868312.jpg)'
+        }}
+       boxShadow={22} 
+       levation={20} 
+       square>
+          <Box
+            sx={{
+              backgroundColor: "white",
+              opacity:0.7,
+              my: 20,
+              mx: 24,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h5"   sx={{ mt: 3, mb: 2 }}>
+              About Us
+            </Typography>
+            <Typography component="h2" variant="h6"   sx={{ mt: 3, mb: 2 }}>
+            We at Miniature Market are dedicated to being a central gaming hub for the community. We offer a family friendly environment for people of all ages to come down and enjoy their favourite games or even learn new ones, while also supplying avid gamers with all the supplies they need to get models on the table!
+            </Typography>
   
-      </div>
-    )
-  }
-  
-  export default About
+   
+
+            
+            </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
+
+export default About
--- src/pages/miniatures/Miniatures.js
+++ src/pages/miniatures/Miniatures.js
@@ -1,6 +1,14 @@
 import axios from 'axios'
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom'
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+import { Card, CardMedia } from '@material-ui/core';
+import { grid } from '@mui/system';
+
 
 const Miniatures = () => {
   const [miniatures, setMiniatures] = useState(null)
@@ -20,27 +28,89 @@ const Miniatures = () => {
   if(!miniatures) return null;
 
   const miniaturesList = miniatures.map(miniature => {
-      return (
-          <div key={miniature._id}>
-              <p>
-                <b>Name: </b> {" "}
+      // return (
+      //     <div key={miniature._id}>
+      //         <p>
+      //           <b>Name: </b> {" "}
+      //           <Link to={`/miniatures/${miniature._id}`}>{miniature.name}</Link>{" "}
+      //           </p>
+      //           <p>
+      //           <b>Faction: </b> {miniature.faction}{" "}
+      //           </p>
+      //         <hr />
+      //     </div>
+          
+      // )
+    return(
+      
+      <div key={miniature._id}>
+      <Paper sx={{ p: 2, margin: 'auto', maxWidth: 500, flexGrow: 1}}>
+      <Grid container spacing={2}>
+        <Grid item>
+          <ButtonBase sx={{ width: 128, height: 128 }}>
+            <CardMedia alt="complex" image="https://i.pinimg.com/originals/2b/d6/bc/2bd6bc81147740daac0319562c47008f.jpg" />
+          </ButtonBase>
+        </Grid>
+        <Grid item xs={12} sm container>
+          <Grid item xs container direction="column" spacing={2}>
+            <Grid item xs>
+              <Typography gutterBottom variant="name" component="div">
+                <p>
                 <Link to={`/miniatures/${miniature._id}`}>{miniature.name}</Link>{" "}
                 </p>
-                <p>
-                <b>Faction: </b> {miniature.faction}{" "}
-                </p>
-              <hr />
-          </div>
+              </Typography>
+              <Typography variant="faction" gutterBottom>
+              <b>Faction: </b> {miniature.faction}{" "}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              <b>ID: </b> {miniature._id}{" "}
+              </Typography>
+            </Grid>
+            <Grid item>
           
-      )
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1" component="div">
+            <b>Price: </b> {miniature.price}{" "}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+    
+    </div>
+    )
   })
 
   return (
+
+    
     <div>
-      <h2>Miniatures</h2>
-      <p>This is the Miniatures Index page</p>
+      <Grid 
+        item 
+        xs={12} 
+        sm={8} 
+        md={12} 
+        component={Card} 
+        sx={{
+          backgroundImage: 'url(https://images7.alphacoders.com/868/868312.jpg)'
+        }}
+       boxShadow={22} 
+       levation={20} 
+       square>
+         <Typography component="h1" variant="h2" align='center' backgroundColor="white">
+           Miniature Catalogue
+         </Typography>
+    
+
+    
       <Link to="create">Create</Link>
     {miniaturesList}
+
+             
+
+    </Grid>
     </div>
   )
 }
